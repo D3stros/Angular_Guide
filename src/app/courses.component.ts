c5f60e6c42988239884b55f70aa4e41f310cf190
Destros <patrick.fallmann@hotmail.com>
2019-09-24 00:41:59 +0200

    Implemented a custom pipe to shorten a text to only 50 characters

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,22 +1,14 @@
 import { Component } from "@angular/core";
 import { CoursesService } from "./courses.service";
+import { SummaryPipe } from "./summary.pipe";
 
 @Component({
   selector: "courses",
   template: `
-    {{ course.title | uppercase | lowercase }} <br />
-    {{ course.students | number }} <br />
-    {{ course.rating | number: "1.1-1" }} <br />
-    {{ course.price | currency: "AUD" }} <br />
-    {{ course.releaseDate | date: "shortDate" }}
+    {{ text | summary: 10 }}
   `
 })
 export class CoursesComponent {
-  course = {
-    title: "The complete angular course",
-    rating: 4.0756,
-    students: 10500,
-    price: 199.95,
-    releaseDate: new Date(2018, 3, 10)
-  };
+  text =
+    "lorem  ipsum sadaaaaaaaaaaaaaaaaaaaadkamdksamdkasmdlkasmdkasmdmaskmdsamdmaskdmamdksadalsmdkasmdlamsdkamdsalksdm";
 }

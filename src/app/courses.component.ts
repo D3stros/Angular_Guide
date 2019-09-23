613ae8d400f281c15ff5111a408161ce1ee05cd1
Destros <patrick.fallmann@hotmail.com>
2019-09-24 00:07:06 +0200

    Logging the event.target.value when enter is pressed

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,11 +4,11 @@ import { CoursesService } from "./courses.service";
 @Component({
   selector: "courses",
   template: `
-    <input (keyup.enter)="onKeyUp()" />
+    <input (keyup.enter)="onKeyUp($event)" />
   `
 })
 export class CoursesComponent {
-  onKeyUp() {
-    console.log("ENTER was pressed");
+  onKeyUp($event) {
+    console.log($event.target.value);
   }
 }

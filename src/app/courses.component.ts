06a73290f336eb5dd147fd0c2c31133b5a9399f3
Destros <patrick.fallmann@hotmail.com>
2019-09-23 22:49:23 +0200

    Using attribute binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,19 +4,15 @@ import { CoursesService } from "./courses.service";
 @Component({
   selector: "courses",
   template: `
-    <h2>{{ 3 + title }}</h2>
-    <ul>
-      <li *ngFor="let course of courses">
-        {{ course }}
-      </li>
-    </ul>
+    <img [src]="imageUrl" />
+    <table>
+      <tr>
+        <td [attr.colspan]="colSpan"></td>
+      </tr>
+    </table>
   `
 })
 export class CoursesComponent {
-  title = "Authors";
-  courses;
-
-  constructor(service: CoursesService) {
-    this.courses = service.getCourses();
-  }
+  imageUrl = "";
+  colSpan = 2;
 }

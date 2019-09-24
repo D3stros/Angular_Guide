1f627d37ab996abb2a112feb69c80185dc06b967
Destros <patrick.fallmann@hotmail.com>
2019-09-24 23:50:05 +0200

    Deleted onInit constructor and ngOnInit

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -5,13 +5,9 @@ import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
   templateUrl: "./favorite.component.html",
   styleUrls: ["./favorite.component.css"]
 })
-export class FavoriteComponent implements OnInit {
+export class FavoriteComponent {
   @Input("isFavorite") isSelected: boolean;
   @Output("change") click = new EventEmitter();
-
-  constructor() {}
-
-  ngOnInit() {}
 
   onClick() {
     this.isSelected = !this.isSelected;

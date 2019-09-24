34e4f77c1e8a02152ef20d69ff3013a186ec6b23
Destros <patrick.fallmann@hotmail.com>
2019-09-24 16:57:09 +0200

    Installed font awesome and implemented a favorite component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(fas);
 
 import { AppComponent } from "./app.component";
 import { CoursesComponent } from "./courses.component";
@@ -9,6 +13,7 @@ import { CoursesService } from "./courses.service";
 import { AuthorsComponent } from "./authors/authors.component";
 import { AuthorsService } from "./authors.service";
 import { SummaryPipe } from "./summary.pipe";
+import { FavoriteComponent } from "./favorite/favorite.component";
 
 @NgModule({
   declarations: [
@@ -16,9 +21,10 @@ import { SummaryPipe } from "./summary.pipe";
     CoursesComponent,
     CourseComponent,
     AuthorsComponent,
-    SummaryPipe
+    SummaryPipe,
+    FavoriteComponent
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, FontAwesomeModule],
   providers: [CoursesService, AuthorsService],
   bootstrap: [AppComponent]
 })

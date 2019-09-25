57b96f8ac8e76ee266a49f7236cddccf97d6ec5c
Destros <patrick.fallmann@hotmail.com>
2019-09-25 20:32:35 +0200

    Implemented an add and a remove button to add and delete elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,13 @@ export class AppComponent {
     { id: 2, name: "course 2" },
     { id: 3, name: "course 3" }
   ];
+
+  onAdd() {
+    this.courses.push({ id: 4, name: "course4" });
+  }
+
+  onRemove(course) {
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
 }

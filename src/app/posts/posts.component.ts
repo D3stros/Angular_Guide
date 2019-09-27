a2adc01c45dc5670953d32130f71e229cee12d7c
Destros <patrick.fallmann@hotmail.com>
2019-09-27 21:23:16 +0200

    Creating data via http post method

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -9,11 +9,20 @@ import { HttpClient } from "@angular/common/http";
 })
 export class PostsComponent {
   posts;
-  constructor(http: HttpClient) {
-    http
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .subscribe(response => {
-        this.posts = response;
-      });
+  private url = "https://jsonplaceholder.typicode.com/posts";
+
+  constructor(private http: HttpClient) {
+    http.get(this.url).subscribe(response => {
+      this.posts = response;
+    });
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post: any = { title: input.value };
+    input.value = "";
+    this.http.post(this.url, JSON.stringify(post)).subscribe(response => {
+      post["id"] = response.id;
+      this.posts.splice(0, 0, post);
+    });
   }
 }

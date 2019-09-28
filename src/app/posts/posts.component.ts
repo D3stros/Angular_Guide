3770e93f7f00fb3691c6d845fafb2a011b4f5952
Destros <patrick.fallmann@hotmail.com>
2019-09-28 15:48:51 +0200

    Removed errors in the component because they should be in the service

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -12,53 +12,29 @@ export class PostsComponent implements OnInit {
   constructor(private service: PostService) {}
 
   ngOnInit() {
-    this.service.getAll().subscribe(
-      posts => (this.posts = posts),
-      error => {
-        alert("An unexpected error occured.");
-        console.log(error);
-      }
-    );
+    this.service.getAll().subscribe(posts => (this.posts = posts));
   }
 
   createPost(input: HTMLInputElement) {
     let post = { title: input.value };
     input.value = "";
 
-    this.service.create(post).subscribe(
-      newPost => {
-        post["id"] = newPost.id;
-        this.posts.splice(0, 0, post);
-      },
-      error => {
-        alert("An unexpected error occured.");
-        console.log(error);
-      }
-    );
+    this.service.create(post).subscribe(newPost => {
+      post["id"] = newPost.id;
+      this.posts.splice(0, 0, post);
+    });
   }
 
   updatePost(post) {
-    this.service.update(post).subscribe(
-      updatedPost => {
-        console.log(updatedPost);
-      },
-      error => {
-        alert("An unexpected error occured.");
-        console.log(error);
-      }
-    );
+    this.service.update(post).subscribe(updatedPost => {
+      console.log(updatedPost);
+    });
   }
 
   deletePost(post) {
-    this.service.delete(post.id).subscribe(
-      () => {
-        let index = this.posts.indexOf(post);
-        this.posts.splice(index, 1);
-      },
-      (error: Response) => {
-        if (error.status === 404) alert("This post has already been deleted.");
-        console.log(error);
-      }
-    );
+    this.service.delete(post.id).subscribe(() => {
+      let index = this.posts.indexOf(post);
+      this.posts.splice(index, 1);
+    });
   }
 }

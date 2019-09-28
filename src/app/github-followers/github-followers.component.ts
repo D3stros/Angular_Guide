33d4e38a71da76e46e01c24f4070ac3de641e2b4
Destros <patrick.fallmann@hotmail.com>
2019-09-28 23:15:05 +0200

    Combining multiple observables via the switchMap operator

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { GithubFollowersService } from "../github-followers.service";
 import { ActivatedRoute } from "@angular/router";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/observable/combineLatest";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/switchMap";
 
 @Component({
   selector: "github-followers",
@@ -15,10 +19,13 @@ export class GithubFollowersComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params => {});
+    Observable.combineLatest([this.route.paramMap, this.route.queryParamMap])
+      .switchMap(combined => {
+        let id = combined[0].get("id");
+        let page = combined[1].get("page");
 
-    this.route.queryParamMap.subscribe(params => {});
-
-    this.service.getAll().subscribe(followers => (this.followers = followers));
+        return this.service.getAll();
+      })
+      .subscribe(followers => (this.followers = followers));
   }
 }
